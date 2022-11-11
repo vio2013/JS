54aeea618d95c74eeb442871569ee842229ac7d1
Andreas Gruenwald <violator2003@mail.ru>
2022-11-11 14:56:58 +0100

    erste

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let persone = {
+    name: "Marry",
+    age: 25,
+    isMarried: true
+
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+/*let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[0]);
+*/
+///alert("fdvbrtgbrt"); 
+//let answer = confirm("Are you here ?");
+//console.log(answer);
+
+//let answer = prompt("Are you here ?", "Ja");
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+
+    console.log(++incr,',',--decr);
+    console.log("2" === 2);
